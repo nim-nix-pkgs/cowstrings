0a90a00984a53251a138091f3744b3c8beeacd94
hugosenari <hugosenari@gmail.com>
2022-09-03 19:12:25 -0300

    chore: re index cowstrings

---
+++ source/cowstrings.nimble
@@ -0,0 +1,10 @@
+# Package
+
+version     = "0.5.0"
+author      = "Antonis Geralis"
+description = "Copy-On-Write string implementation according to nim-lang/RFCs#221"
+license     = "MIT"
+
+# Deps
+
+requires "nim >= 1.7.1"
---
+++ source/tests/tcowstrings.nim
@@ -0,0 +1,34 @@
+import cowstrings
+import std/isolation
+
+proc main =
+  block:
+    var a = initStringOfCap(10)
+    a.add 'h'
+    var b = a
+    b.add a
+    b.add 'w'
+    echo a.toCStr # prevent sink
+    echo b.toCStr
+  block:
+    var a: String
+    a.add 'h'
+    a.add 'e'
+    echo a.toCStr
+  block:
+    var a: Isolated[String]
+    var b: String
+    b.add 'w'
+    b.add 'o'
+    a = isolate b
+    #b.add 'r'
+    let c = extract a
+    echo c.toCStr
+  block:
+    let a = cstring"World".toStr
+    var b = a
+    prepareStrMutation(b)
+    b[0] = 'P'
+    assert a == cstring"World".toStr
+
+main()
